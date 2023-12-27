5e71d7e3fa92c4060a9e46afbccf740f6b090b00
Kyle Kim <dkim577@wisc.edu>
2023-12-27 23:19:47 +0900

    Updates

---
+++ precache-manifest.812de228cf01c433aec5f88edda47604.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2f9729c8370d236c79decbd17314a9",
+    "url": "/React_NomardCoder/index.html"
+  },
+  {
+    "revision": "24376076a018867ba030",
+    "url": "/React_NomardCoder/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "b6ae14ef2eba0ddfa73d",
+    "url": "/React_NomardCoder/static/js/2.50ca7ba2.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/React_NomardCoder/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "24376076a018867ba030",
+    "url": "/React_NomardCoder/static/js/main.69f52fe8.chunk.js"
+  },
+  {
+    "revision": "0bd01fe3088a3ed31eeb",
+    "url": "/React_NomardCoder/static/js/runtime~main.17a708d5.js"
+  }
+]);
